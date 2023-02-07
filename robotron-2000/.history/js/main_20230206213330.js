42fdc4f9ef3ed2b7bf28cc77d07bb7ef9cb29a2c
calesco <vcalesco@gmail.com>
2023-02-06 21:41:05 -0300

    portfolio

---
+++ robotron-2000/.history/js/main_20230206213330.js
@@ -0,0 +1,9 @@
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+
+robotron.addEventListener("click", () => {
+    braco.value += 1
+})
+
---
+++ robotron-2000/.history/js/main_20230206213544.js
@@ -0,0 +1,9 @@
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+
+robotron.addEventListener("click", (evento) => {
+    braco.value += 1
+});
+
