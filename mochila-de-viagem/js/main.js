9ff88e0a69af0baad1fdb40c2ec6cfed5a7e5d4c
calesco <vcalesco@gmail.com>
2023-02-08 23:34:23 -0300

    armazenar localStorage

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,10 +1,17 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 
+
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) { 
@@ -18,3 +25,6 @@ function criaElemento(nome, quantidade) {
    novoItem.innerHTML += nome 
 
    lista.appendChild(novoItem)
+   localStorage.setItem("nome", nome) 
+   localStorage.setItem("quantidade", quantidade) 
+}
