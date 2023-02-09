e9d791291f6d11f5d04fa706b645ef280a605ca5
calesco <vcalesco@gmail.com>
2023-02-08 23:34:34 -0300

    armazenar localStorage

---
+++ mochila-de-viagem/.history/js/main_20230208233151.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
+
+    criaElemento(nome, quantidade)
+
+    nome = ""
+    quantidade = ""
+})
+
+function criaElemento(nome, quantidade) { 
+   const novoItem = document.createElement('li')
+   novoItem.classList.add("item")
+
+   const numeroItem = document.createElement('strong')
+   numeroItem.innerHTML = quantidade
+   
+   novoItem.appendChild(numeroItem)
+   novoItem.innerHTML += nome 
+
+   lista.appendChild(novoItem)
---
+++ mochila-de-viagem/.history/js/main_20230208233214.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome, quantidade)
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(nome, quantidade) { 
+   const novoItem = document.createElement('li')
+   novoItem.classList.add("item")
+
+   const numeroItem = document.createElement('strong')
+   numeroItem.innerHTML = quantidade
+   
+   novoItem.appendChild(numeroItem)
+   novoItem.innerHTML += nome 
+
+   lista.appendChild(novoItem)
